67e58bd88b8ff7490182f5579b32fceb1f8681e7
dsociative <admin@geektech.ru>
2013-07-09 15:23:00 +0400

    find_packages in setup

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='social_billing',
       description='service for processing social payment',
       author='dsociative',
       author_email='admin@geektech.ru',
-      packages=['social_billing', 'social_billing.web',
-                'social_billing.web.handler',
-                'social_billing.engine', 'social_billing.engine.handler'],
+      packages=find_packages(),
       package_data={'social_billing': ['translations/*.csv']},
       dependency_links=[
           "http://github.com/dsociative/ztest/tarball/master#egg=ztest-0.0.0",
